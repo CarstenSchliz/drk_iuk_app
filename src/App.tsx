722c3d64a2fbd6698e1ea34df58a4efeb7e88654
CarstenSchliz <schlizcarsten@gmail.com>
2025-08-15 22:51:46 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,1 @@
-import { useState } from 'react'
-
-export default function App() {
-  const [count, setCount] = useState(0)
-  return (
-    <div style={{textAlign:'center', padding:'2rem'}}>
-      <img src="/icons/drk-sprechfunk-icon-192.png" alt="DRK Blitz" style={{width:160}} />
-      <h1>DRK IuK Lernmodule</h1>
-      <p>Willkommen in der Lern-App der Fachgruppe Information &amp; Kommunikation.</p>
-      <button onClick={() => setCount(c => c + 1)}>Klick mich ({count})</button>
-    </div>
-  )
-}
+<img src="/icons/blitz-123.png" alt="DRK Blitz" style={{ width: 160, height: 'auto' }} />
